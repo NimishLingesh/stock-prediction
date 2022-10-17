a47333b6092a2fe22b608cf91eff832d4a4bd8a4
shwethabhandary <57583946+shwethabhandary@users.noreply.github.com>
2022-10-17 00:36:36 -0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,17 @@
 import pandas as pd
 import numpy as np
+
+#Here we read all the data from the csv file from drive, keep the backup of the data and see the informations of the same.
+stock_data= pd.read_csv('/content/stock_list.csv', header=0, na_values='?')
+stock_data_backup= stock_data.copy()
+stock_data.info()
+print(stock_data.isna().any(),"\n")
+#We can now see that the columns that require cleaning are NewMarketSegment, TradeDate, Close, IssuedShares and MarketCapitalization
+
+print("NewMarketSegment-> Null Values= ",stock_data['NewMarketSegment'].isna().sum())
+print("TradeDate-> Null Values= ",stock_data['TradeDate'].isna().sum())
+print("Close-> Null Values= ",stock_data['Close'].isna().sum())
+print("IssuedShares-> Null Values= ",stock_data['IssuedShares'].isna().sum())
+print("MarketCapitalization-> Null Values= ",stock_data['MarketCapitalization'].isna().sum())
+# here we can see that NewMarketSegment has 645, TradeDate has 296, Close has 296, IssuedShares has 296 and MarketCapitalization has 296 data values missing.
+
